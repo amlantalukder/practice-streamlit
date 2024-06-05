ab8ce770aa1f615ff9b8747f32a14a6389714611
Amlan Talukder <amlanaccount@gmail.com>
2024-06-05 13:24:26 -0400

    Updated design

--- codes/app.py
+++ codes/app.py
@@ -8,9 +8,10 @@ df = px.data.tips()
 st.set_page_config(layout="wide")
 
 def figUpdateLayout(fig):
-    fig.update_layout(title_font_size = 40)
-    fig.update_yaxes(title_font_size = 30, tickfont_size = 20)
-    fig.update_xaxes(title_font_size = 30, tickfont_size = 20)
+    base_font_size = 12
+    fig.update_layout(title_font_size = base_font_size*2)
+    fig.update_yaxes(title_font_size = base_font_size*1.5, tickfont_size = base_font_size*1.5)
+    fig.update_xaxes(title_font_size = base_font_size*1.5, tickfont_size = base_font_size*1.5)
     return fig
 
 with st.container(border=True):
@@ -27,7 +28,7 @@ with st.container(height=1000):
 
     col1.write(f"## Total customers: {df.shape[0]}")
     fig = px.sunburst(df, path=['sex', 'day', 'time'], values='total_bill', color='day', width=800, height=600)
-    col1.plotly_chart(fig)
+    col1.plotly_chart(fig, use_container_width=True)
 
     d = st.sidebar.selectbox("Day", df['day'].unique())
     p = st.sidebar.selectbox("Time", df['time'].unique())
@@ -37,14 +38,14 @@ with st.container(height=1000):
         
         df_sub = df[(df['time'] == p) & (df['day'] == d)]
 
-        fig = px.violin(df_sub, box=True, x='sex', y='tip', width=800, height=600, title="Tip amount based on gender")
+        fig = px.violin(df_sub, box=True, x='sex', y='tip', title="Tip amount based on gender")
         fig = figUpdateLayout(fig)
-        col1.plotly_chart(fig)
+        col1.plotly_chart(fig, use_container_width=True)
 
-        fig = px.box(df_sub, x='size', y='tip', width=800, height=600, title="Tip amount based on party size")
+        fig = px.box(df_sub, x='size', y='tip', title="Tip amount based on party size")
         fig = figUpdateLayout(fig)
-        col2.plotly_chart(fig)
+        col2.plotly_chart(fig, use_container_width=True)
 
-        fig = px.scatter(df_sub, x='total_bill', y='tip', width=800, height=600, title="Tip amount based on total bill")
+        fig = px.scatter(df_sub, x='total_bill', y='tip', title="Tip amount based on total bill")
         fig = figUpdateLayout(fig)
-        col2.plotly_chart(fig)
+        col2.plotly_chart(fig, use_container_width=True)
