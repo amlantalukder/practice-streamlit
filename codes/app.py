89d87f09a1bf2de3f6d7acee2300567e152533e9
Amlan Talukder <amlanaccount@gmail.com>
2024-06-05 11:38:56 -0400

    Updated design, changed dataset

--- codes/app.py
+++ codes/app.py
@@ -4,31 +4,34 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 
-st.set_page_config(layout="wide")
-cont = st.container(height=1000)
-
-cont.write("""
-    # Practice app to learn streamlit
-""")
-
 df = px.data.tips()
-cont.table(df.head())
 
-col1, col2 = cont.columns(2)
+st.set_page_config(layout="wide")
+with st.container():
 
-col1.write(f"## Total customers: {df.shape[0]}")
-fig = px.sunburst(df, path=['sex', 'day', 'time'], values='total_bill', color='day')
-col1.plotly_chart(fig)
+    st.write("""
+        # Practice app to learn streamlit
+    """)
 
-x = st.sidebar.selectbox("Time", df['time'].unique())
+    st.table(df.head())
 
-with st.spinner(text="In progress"):
-    
-    fig = px.box(df[df['time'] == x], x='sex', y='tip')
-    col2.plotly_chart(fig)
+with st.container(height=1000):
 
-    fig = px.box(df[df['time'] == x], x='size', y='tip')
-    col2.plotly_chart(fig)
+    col1, col2 = st.columns(2)
 
-    fig = px.scatter(df[df['time'] == x], x='total_bill', y='tip')
-    col2.plotly_chart(fig)
+    col1.write(f"## Total customers: {df.shape[0]}")
+    fig = px.sunburst(df, path=['sex', 'day', 'time'], values='total_bill', color='day')
+    col1.plotly_chart(fig)
+
+    x = st.sidebar.selectbox("Time", df['time'].unique())
+
+    with st.spinner(text="In progress"):
+        
+        fig = px.box(df[df['time'] == x], x='sex', y='tip')
+        col1.plotly_chart(fig)
+
+        fig = px.box(df[df['time'] == x], x='size', y='tip')
+        col2.plotly_chart(fig)
+
+        fig = px.scatter(df[df['time'] == x], x='total_bill', y='tip')
+        col2.plotly_chart(fig)
