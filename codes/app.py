ba806a8384a67a305d616137ac183b6de4c330d9
Amlan Talukder <amlanaccount@gmail.com>
2024-06-04 20:09:49 -0400

    Added github actions workflow

--- codes/app.py
+++ codes/app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.write("""
-    # Practice app to learn streamlit(U)
+    # Practice app to learn streamlit
 """)
 
 df = sns.load_dataset("titanic")
