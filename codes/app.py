97e97c1fed3c38ea8ef8e671551ea89454d57164
Amlan Talukder <amlanaccount@gmail.com>
2024-06-05 11:31:07 -0400

    Updated design, changed dataset

--- codes/app.py
+++ codes/app.py
@@ -1,22 +1,34 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 import seaborn as sns
 
-st.write("""
+st.set_page_config(layout="wide")
+cont = st.container(height=1000)
+
+cont.write("""
     # Practice app to learn streamlit
 """)
 
-df = sns.load_dataset("titanic")
-df
-x = st.sidebar.selectbox("Passenger Class", df['class'].unique())
+df = px.data.tips()
+cont.table(df.head())
+
+col1, col2 = cont.columns(2)
+
+col1.write(f"## Total customers: {df.shape[0]}")
+fig = px.sunburst(df, path=['sex', 'day', 'time'], values='total_bill', color='day')
+col1.plotly_chart(fig)
+
+x = st.sidebar.selectbox("Time", df['time'].unique())
 
 with st.spinner(text="In progress"):
-    fig, ax = plt.subplots(figsize=(10, 4))
-    ax = sns.boxplot(df[df['class'] == x], x='sex', y='age')
-    st.pyplot(fig)
+    
+    fig = px.box(df[df['time'] == x], x='sex', y='tip')
+    col2.plotly_chart(fig)
 
-    fig, ax = plt.subplots(figsize=(10, 4))
-    ax = sns.boxplot(df[df['class'] == x], x='embarked', y='age')
-    st.pyplot(fig)
+    fig = px.box(df[df['time'] == x], x='size', y='tip')
+    col2.plotly_chart(fig)
 
+    fig = px.scatter(df[df['time'] == x], x='total_bill', y='tip')
+    col2.plotly_chart(fig)
